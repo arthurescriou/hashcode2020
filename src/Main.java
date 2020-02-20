c8bf5d6936893ed7be52395a42e73c4f3627fa61
yfo <yohann.fouquer@lesfurets.com>
2020-02-20 18:44:39 +0100

    ferbnijurefo;binu;ijbnreq;giuhr

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) by Courtanet, All Rights Reserved.
+ */
+
+import java.io.IOException;
+import java.nio.file.*;
+
+public class Main {
+
+    private static void loadData(String file){
+        try {
+            Files.lines(Paths.get(""));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
