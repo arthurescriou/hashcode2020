f85fc7b176a7e07fa16eadfffbc586362cd213d7
yfo <yohann.fouquer@lesfurets.com>
2020-02-20 19:43:28 +0100

    gertouhinp

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,22 @@
  * Copyright (C) by Courtanet, All Rights Reserved.
  */
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.*;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import com.sun.org.apache.xpath.internal.operations.Or;
+
 public class Main {
 
-    public static final String A = "/home/yfouquer/hashcode/data/a_example.txt";
-    public static final String B = "/home/yfouquer/hashcode/data/b_read_on.txt";
-    public static final String C = "/home/yfouquer/hashcode/data/c_incunabula.txt";
-    public static final String D = "/home/yfouquer/hashcode/data/d_tough_choices.txt";
-    public static final String E = "/home/yfouquer/hashcode/data/e_so_many_books.txt";
-    public static final String F = "/home/yfouquer/hashcode/data/f_libraries_of_the_world.txt";
+    public static final String A = "data/a_example.txt";
+    public static final String B = "data/b_read_on.txt";
+    public static final String C = "data/c_incunabula.txt";
+    public static final String D = "data/d_tough_choices.txt";
+    public static final String E = "data/e_so_many_books.txt";
+    public static final String F = "data/f_libraries_of_the_world.txt";
     static List<Books> books = new ArrayList<>();
     static List<Library> libraries = new ArrayList<>();
     private static int daysForScanning;
@@ -55,8 +57,34 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        loadData(B);
+        loadData(A);
 
+        final int[] accuDaysSignUp = { 0 };
+        List<Library> OrderedSelectedLibrary =
+                libraries.stream().sorted(Comparator.comparingDouble(Main::scoreMoyenLibrary)).filter(library -> {
+                    accuDaysSignUp[0] += library.getSignUpProcess();
+                    return accuDaysSignUp[0] < daysForScanning;
+                }).collect(Collectors.toList());
 
+        printResult(OrderedSelectedLibrary);
+
+    }
+
+    private static void printResult(List<Library> lib) {
+        try {
+
+            FileWriter writer = new FileWriter(new File(A + "_result.txt"));
+            writer.write(lib.size()+"");
+            writer.write("\n");
+            writer.write(lib.stream().map(libr -> libr.getId() + " " + libr.getContainsBooks().size() + "\n" + libr.getContainsBooks().values().stream().sorted(Comparator.comparingInt(Books::getScore)).map(books34 -> books34.getId() +"").collect(Collectors.joining(" ")) + "\n").collect(Collectors.joining()));
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static double scoreMoyenLibrary(Library library) {
+        int scoreTotal = library.getContainsBooks().values().stream().mapToInt(Books::getScore).sum();
+        return scoreTotal / (0.0 + library.getSignUpProcess() + (books.size() + 0.0 / library.getCanShipPerDay()));
     }
 }
